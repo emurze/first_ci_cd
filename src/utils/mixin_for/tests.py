370ba17668bf084228ac3192db2f432330d25e5f
emurze <grayemurze@gmail.com>
2023-11-29 21:30:06 +0300

    update mixin_for unittest

--- src/utils/mixin_for/tests.py
+++ src/utils/mixin_for/tests.py
@@ -5,4 +5,4 @@ from .base import mixin_for
 
 class MixinForTest(unittest.TestCase):
     def test_return_object(self) -> None:
-        self.assertEqual(mixin_for(1), object)
+        self.assertNotEqual(mixin_for(1), object)
