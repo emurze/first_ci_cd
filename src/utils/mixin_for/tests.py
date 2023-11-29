80389c0d9e0a2b9527d5b065c4bc1c02c0953306
emurze <grayemurze@gmail.com>
2023-11-29 22:08:21 +0300

    fix error

--- src/utils/mixin_for/tests.py
+++ src/utils/mixin_for/tests.py
@@ -5,5 +5,4 @@ from .base import mixin_for
 
 class MixinForTest(unittest.TestCase):
     def test_return_object(self) -> None:
-        print('hi')
-        self.assertEqual(mixin_for(1), 1)
+        self.assertEqual(mixin_for(1), object)
