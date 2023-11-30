92de2b0d131a7b62a652641ad684668b36c7d414
emurze <grayemurze@gmail.com>
2023-11-30 21:46:35 +0300

    hope

---
+++ tests/base.py
@@ -0,0 +1,11 @@
+from tests.libs import MyLiveServerTestCase, get_driver
+
+
+class BaseTest(MyLiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = get_driver()
+
+    def test_title(self) -> None:
+        self.driver.get(self.live_server_url)
+
+        self.assertIn('Django', self.driver.title)
