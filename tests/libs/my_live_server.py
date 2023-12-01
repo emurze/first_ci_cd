5e4d072b357167fe3f4e49862dea59565e5fe963
emurze <grayemurze@gmail.com>
2023-12-01 21:21:01 +0300

    ergerg

--- tests/libs/my_live_server.py
+++ tests/libs/my_live_server.py
@@ -5,8 +5,8 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class MyLiveServerTestCase(StaticLiveServerTestCase):
-    if os.getenv('STAGING_SERVER'):
-        host = 'localhost'
-    else:
-        host = sock.gethostbyname(sock.gethostname())
+    # if os.getenv('STAGING_SERVER'):
+    #     host = 'localhost'
+    # else:
+    host = sock.gethostbyname(sock.gethostname())
     port = 8088
