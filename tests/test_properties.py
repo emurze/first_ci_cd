4e91caa4a0783ddebe21e873a262975d9d00feb2
emurze <grayemurze@gmail.com>
2023-12-01 16:49:25 +0300

    add build

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -8,12 +8,13 @@ class BaseTest(MyLiveServerTestCase):
     def test_title(self) -> None:
         print(self.live_server_url)
 
-        self.driver.get('http://0.0.0.0:80')
+        self.driver.get('https://google.com')
+        print(self.driver.title)
 
+        self.driver.get('http://0.0.0.0:80')
         print(self.driver.title)
 
         self.driver.get('http://0.0.0.0:8080')
-
         print(self.driver.title)
 
         self.driver.get('http://0.0.0.0:8088')
