8bf88eeba446a87d926a4ef010ddc84b5d37e74b
emurze <grayemurze@gmail.com>
2023-12-01 20:46:04 +0300

    ergerg

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -6,17 +6,6 @@ class BaseTest(MyLiveServerTestCase):
         self.driver = get_driver()
 
     def test_title(self) -> None:
-        print(self.live_server_url)
-
-        self.driver.get('https://google.com')
-        print(self.driver.title)
-
-        self.driver.get('http://0.0.0.0:80')
-        print(self.driver.title)
-
-        self.driver.get('http://0.0.0.0:8080')
-        print(self.driver.title)
-
-        self.driver.get('http://0.0.0.0:8088')
+        self.driver.get(self.live_server_url)
 
         self.assertIn('Django', self.driver.title)
