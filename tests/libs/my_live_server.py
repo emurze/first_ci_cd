cfc9ecff23b4448ef878a94af6fdf6f8895c322b
emurze <grayemurze@gmail.com>
2023-12-01 15:34:59 +0300

    show staging server url

--- tests/libs/my_live_server.py
+++ tests/libs/my_live_server.py
@@ -6,4 +6,4 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class MyLiveServerTestCase(StaticLiveServerTestCase):
     host = os.getenv('STAGING_SERVER', sock.gethostbyname(sock.gethostname()))
-    port = 8084
+    port = 8088
