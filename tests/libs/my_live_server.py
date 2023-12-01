b7446be8496cc90ff237ab6f84ad52bbc2f9239a
emurze <grayemurze@gmail.com>
2023-12-01 21:34:46 +0300

    fix end-to-end pipeline part

--- tests/libs/my_live_server.py
+++ tests/libs/my_live_server.py
@@ -1,12 +1,8 @@
-import os
 import socket as sock
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class MyLiveServerTestCase(StaticLiveServerTestCase):
-    # if os.getenv('STAGING_SERVER'):
-    #     host = 'localhost'
-    # else:
     host = sock.gethostbyname(sock.gethostname())
     port = 8088
