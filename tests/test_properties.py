91164144d4f6725d1c8fb8995b6fb4be4b388db6
emurze <grayemurze@gmail.com>
2023-12-01 21:29:48 +0300

    ergerg

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -6,8 +6,6 @@ class BaseTest(MyLiveServerTestCase):
         self.driver = get_driver()
 
     def test_title(self) -> None:
-        self.driver.get('https://github.com')
-
-        self.driver.get(self.live_server_url)
+        self.driver.get(f'{self.live_server_url}/admin')
 
         self.assertIn('Django', self.driver.title)
